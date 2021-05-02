da938a695beee93b47fab23c914612b0ce1a4e38
robertov92 <vil13004@byui.edu>
2021-05-01 20:04:10 -0500

    Some changes :P

--- routes/ta02.js
+++ routes/ta02.js
@@ -1,15 +1,59 @@
 //TA02 PLACEHOLDER
-// Remember, you can make more of these placeholders yourself! 
+// This app makes use of basic principles of backend dev with node and express! 
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    res.render('pages', { 
-        title: 'Team Activity 02', 
-        path: '/ta02', // For pug, EJS 
-        activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
+const users = [];
+
+router.post('/addUser', (req, res, next) => {
+    let repeated = 0;
+    let message = "";
+    users.forEach(element => {
+        if (element == req.body.user) {
+            repeated = 1;
+            message = "Repeated User!";
+        }
+    });
+    if (repeated != 1) users.push(req.body.user);
+    res.render('pages/ta02', {
+        title: 'Team Activity 02',
+        path: '/ta02',
+        users: users,
+        message: message
     });
 });
 
-module.exports = router;
+router.post('/removeUser', (req, res, next) => {
+    users.splice(req.body.userToDelete, 1);
+    res.render('pages/ta02', {
+        title: 'Team Activity 02',
+        path: '/ta02',
+        users: users
+    });
+});
+
+router.post('/removeUser2', (req, res, next) => {
+    const index = users.indexOf(req.body.userToDelete);
+    let message = "";
+    if (index == -1) message = "User not found";
+    else users.splice(index, 1);
+    res.render('pages/ta02', {
+        title: 'Team Activity 02',
+        path: '/ta02',
+        users: users,
+        message: message
+    });
+});
+
+router.get('/', (req, res, next) => {
+    res.render('pages/ta02', {
+        title: 'Team Activity 02',
+        path: '/ta02', // For pug, EJS 
+        activeTA03: true, // For HBS
+        contentCSS: true, // For HBS
+        users: users
+    });
+});
+
+exports.router = router;
+exports.users = users;
