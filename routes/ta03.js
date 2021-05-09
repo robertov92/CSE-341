820c951b24234e880ee8f81889d0525ad0b4b6a3
robertov92 <vil13004@byui.edu>
2021-05-08 19:25:15 -0500

    ta03

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,6 +1,29 @@
 //TA03 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
+const https = require('https');
+
+let url = 'https://byui-cse.github.io/cse341-course/lesson03/items.json'
+let body = "";
+
+https.get(url, function (res) {
+
+    res.on("data", (chunk) => {
+        body += chunk;
+    });
+
+    res.on("end", () => {
+        try {
+            let json = JSON.parse(body);
+            
+        } catch (error) {
+            console.error(error.message);
+        };
+    });
+
+}).on("error", (error) => {
+    console.error(error.message);
+});
 
 router.get('/',(req, res, next) => {
     res.render('pages/ta03', { 
@@ -8,6 +31,7 @@ router.get('/',(req, res, next) => {
         path: '/ta03', // For pug, EJS 
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
+        body: JSON.parse(body)
     });
 });
 
