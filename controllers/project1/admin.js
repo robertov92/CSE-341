5ab44976c17c590faf9e818dbe8baa6d02664716
robertov92 <vil13004@byui.edu>
2021-06-01 12:32:21 -0500

    adding more validation3

--- controllers/project1/admin.js
+++ controllers/project1/admin.js
@@ -1,4 +1,5 @@
 const Product = require('../../models/project1/product');
+const { validationResult } = require('express-validator/check');
 
 // gets products to show in the 'Admin Products' page
 exports.getAdminProds = (req, res, next) => {
@@ -16,7 +17,10 @@ exports.getAdminProds = (req, res, next) => {
 exports.getAddProduct = (req, res, next) => {
     res.render('pages/project1/add-product', {
         pageTitle: 'Add Product',
-        editing: false
+        editing: false,
+        hasError: false,
+        errorMessage: null,
+        validationErrors: []
     });
 };
 
@@ -26,6 +30,19 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).render('pages/project1/add-product', {
+            pageTitle: 'Add Product',
+            editing: false,
+            hasError: true,
+            product: { title, imageUrl, price, description },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+        });
+    }
+
     const product = new Product({
         title: title,
         price: price,
@@ -33,6 +50,7 @@ exports.postAddProduct = (req, res, next) => {
         imageUrl: imageUrl,
         userId: req.user
     });
+
     product
         .save()
         .then(() => {
@@ -68,7 +86,10 @@ exports.getEditProduct = (req, res, next) => {
             res.render('pages/project1/add-product', {
                 pageTitle: 'Edit Product',
                 editing: editMode,
-                product: product
+                product: product,
+                hasError: false,
+                errorMessage: null,
+                validationErrors: []
             });
         })
         .catch(err => console.log(err));
@@ -81,6 +102,18 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).render('pages/project1/add-product', {
+            pageTitle: 'Edit Product',
+            editing: true,
+            hasError: true,
+            product: { title: updatedTitle, imageUrl: updatedImageUrl, price: updatedPrice, description: updatedDesc, _id:prodId },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+        });
+    }
 
     Product.findById(prodId)
         .then(product => {
--- routes/project1/index.js
+++ routes/project1/index.js
@@ -1,12 +1,11 @@
 const project1Routes = require('express').Router();
 const { check, body } = require('express-validator/check');
-
 const shopControllers = require('../../controllers/project1');
 const adminControllers = require('../../controllers/project1/admin');
 const authControllers = require('../../controllers/project1/auth');
 const isAuth = require('../../middleware/is-auth');
-
 const User = require('../../models/project1/user');
+
 
 
 
@@ -45,11 +44,22 @@ project1Routes.post('/reset', authControllers.postReset);
 
 // admin contorller operations
 project1Routes.get('/edit-product/:productId', isAuth, adminControllers.getEditProduct);
-project1Routes.post('/edit-product', isAuth, adminControllers.postEditProduct);
+project1Routes.post('/edit-product', isAuth, [
+    body('title').isString().isLength({ min: 3 }).trim(),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description').isLength({ min: 8, max: 400 }).trim()
+], adminControllers.postEditProduct);
 project1Routes.get('/adminProds', isAuth, adminControllers.getAdminProds);
 project1Routes.get('/add-product', isAuth, adminControllers.getAddProduct);
-project1Routes.post('/add-product', isAuth, adminControllers.postAddProduct);
+project1Routes.post('/add-product', isAuth, [
+    body('title').isString().isLength({ min: 3 }).trim(),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description').isLength({ min: 8, max: 400 }).trim()
+], adminControllers.postAddProduct);
 project1Routes.post('/delete-product', isAuth, adminControllers.postDeleteProduct);
+
 
 
 // shop controller operations
@@ -61,5 +71,8 @@ project1Routes.post('/create-order', isAuth, shopControllers.postOrder);
 project1Routes.get('/orders', isAuth, shopControllers.getOrders);
 project1Routes.get('/:productId', shopControllers.getProduct);
 project1Routes.get('/', shopControllers.getIndex);
+
+
+
 
 module.exports = project1Routes;
